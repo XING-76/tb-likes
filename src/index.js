56493dd56dac7ff1b1635035d57588c2bd205357
LAPTOP-VF17R41D\JACK <jackjohnton789@gmail.com>
2022-01-23 15:47:56 +0800

    fix path, use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store'
 import { Provider } from 'react-redux'
 import App from './App'
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 , document.getElementById('root'))
