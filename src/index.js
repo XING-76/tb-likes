6fe96a4c1a765e8749aaca0e992e6874cef34f4b
LAPTOP-VF17R41D\JACK <jackjohnton789@gmail.com>
2022-01-23 15:07:16 +0800

    test HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import store from './redux/store'
 import { Provider } from 'react-redux'
 import App from './App'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 , document.getElementById('root'))
